280ac95a12e217e007e2cf9ba38e1938bfee1076
Kacper Żuk <kacper.zuk@neti-soft.com>
2025-07-28 14:16:37 +0200

    Fix deadlock on migrations

--- golem-base-indexer/golem-base-indexer-migration/src/m20220101_000001_create_table.rs
+++ golem-base-indexer/golem-base-indexer-migration/src/m20220101_000001_create_table.rs
@@ -32,6 +32,7 @@ create type golem_base_entity_status_type as enum (
 create table golem_base_operations (
     entity_key bytea not null,
     sender bytea not null,
+    recipient bytea not null,
     operation golem_base_operation_type not null,
     data bytea,
     btl numeric(21,0), -- we must fit uint64
@@ -112,9 +113,14 @@ create table golem_base_pending_transaction_operations (
     hash bytea not null primary key references transactions (hash),
     inserted_at timestamp NOT NULL DEFAULT (now())
 );
+
+create table golem_base_pending_transaction_cleanups (
+    hash bytea not null primary key references transactions (hash),
+    inserted_at timestamp NOT NULL DEFAULT (now())
+);
         "#;
 
-        let create_function = Statement::from_string(
+        let create_function_new_tx = Statement::from_string(
             DatabaseBackend::Postgres,
             r#"
 create or replace function golem_base_queue_transaction_processing()
@@ -123,22 +129,71 @@ create or replace function golem_base_queue_transaction_processing()
 as
 $$
 begin
-    if new.to_address_hash in ('\x4200000000000000000000000000000000000015', '\x0000000000000000000000000000000060138453') then
-        insert into golem_base_pending_transaction_operations (hash) values (new.hash);
-    end if;
+    insert into golem_base_pending_transaction_operations (hash) values (new.hash);
     return new;
 end;
 $$
 "#,
         );
 
-        let create_trigger = Statement::from_string(
+        let create_function_dropped_tx = Statement::from_string(
             DatabaseBackend::Postgres,
             r#"
-create trigger golem_base_queue_transaction_processing
+create or replace function golem_base_queue_transaction_cleanup()
+    returns trigger
+    language plpgsql
+as
+$$
+begin
+    insert into golem_base_pending_transaction_cleanups (hash) values (new.hash);
+    return new;
+end;
+$$
+"#,
+        );
+
+        let create_insert_trigger = Statement::from_string(
+            DatabaseBackend::Postgres,
+            r#"
+create trigger golem_base_handle_tx_insert
 after insert on transactions
 for each row
+when (
+    new.to_address_hash in ('\x4200000000000000000000000000000000000015', '\x0000000000000000000000000000000060138453')
+    and new.block_hash is not null
+    and new.status = 1
+)
 execute function golem_base_queue_transaction_processing();
+"#,
+        );
+
+        let create_update_trigger = Statement::from_string(
+            DatabaseBackend::Postgres,
+            r#"
+create trigger golem_base_handle_tx_update
+after update on transactions
+for each row
+when (
+    new.to_address_hash in ('\x4200000000000000000000000000000000000015', '\x0000000000000000000000000000000060138453')
+    and (old.block_hash is null or old.status = 0)
+    and (new.block_hash is not null and new.status = 1)
+)
+execute function golem_base_queue_transaction_processing();
+"#,
+        );
+
+        let create_cleanup_trigger = Statement::from_string(
+            DatabaseBackend::Postgres,
+            r#"
+create trigger golem_base_handle_tx_update_for_cleanup
+after update on transactions
+for each row
+when (
+    new.to_address_hash in ('\x4200000000000000000000000000000000000015', '\x0000000000000000000000000000000060138453')
+    and new.block_hash is null
+    and old.block_hash is not null
+)
+execute function golem_base_queue_transaction_cleanup();
 "#,
         );
 
@@ -158,8 +213,11 @@ where
             .split(';')
             .map(|s| Statement::from_string(DatabaseBackend::Postgres, s))
             .collect();
-        stmts.push(create_function);
-        stmts.push(create_trigger);
+        stmts.push(create_function_new_tx);
+        stmts.push(create_function_dropped_tx);
+        stmts.push(create_update_trigger);
+        stmts.push(create_insert_trigger);
+        stmts.push(create_cleanup_trigger);
         stmts.push(copy_data);
 
         let txn = manager.get_connection().begin().await?;
@@ -174,13 +232,17 @@ where
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let sql = r#"
-            drop trigger if exists golem_base_queue_transaction_processing on golem_base_pending_transaction_operations;
-            drop function if exists golem_base_queue_transaction_processing cascade;
+            drop trigger if exists golem_base_handle_tx_create on transactions;
+            drop trigger if exists golem_base_handle_tx_update on transactions;
+            drop trigger if exists golem_base_handle_tx_update_for_cleanup on transactions;
+            drop function if exists golem_base_queue_transaction_cleanup;
+            drop function if exists golem_base_queue_transaction_processing;
             drop table if exists golem_base_pending_transaction_operations;
             drop table if exists golem_base_string_annotations;
             drop table if exists golem_base_numeric_annotations;
             drop table if exists golem_base_entities;
             drop table if exists golem_base_operations;
+            drop table if exists golem_base_pending_transaction_cleanups;
             drop type if exists golem_base_operation_type;
             drop type if exists golem_base_entity_status_type;
         "#;
