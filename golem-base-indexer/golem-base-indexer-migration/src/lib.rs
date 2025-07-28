be01a7428c15f365ff8f4654ba1ceaf23db2a694
Kacper Żuk <kacper.zuk@neti-soft.com>
2025-07-28 14:16:37 +0200

    Fix deadlock on migrations

--- golem-base-indexer/golem-base-indexer-migration/src/lib.rs
+++ golem-base-indexer/golem-base-indexer-migration/src/lib.rs
@@ -2,17 +2,13 @@ pub use sea_orm_migration::prelude::*;
 use sea_orm_migration::sea_orm::{Statement, TransactionTrait};
 
 mod m20220101_000001_create_table;
-mod m20250723_214557_reorgs;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![
-            Box::new(m20220101_000001_create_table::Migration),
-            Box::new(m20250723_214557_reorgs::Migration),
-        ]
+        vec![Box::new(m20220101_000001_create_table::Migration)]
     }
 
     fn migration_table_name() -> DynIden {
