5854d13be37243bd5969b394153365dd9c90455e
Kacper Żuk <kacper.zuk@neti-soft.com>
2025-08-01 11:22:26 +0200

    Fix reorg handling

--- golem-base-indexer/golem-base-indexer-logic/src/repository/transactions.rs
+++ golem-base-indexer/golem-base-indexer-logic/src/repository/transactions.rs
@@ -16,3 +16,16 @@ pub async fn finish_tx_processing<T: ConnectionTrait>(db: &T, tx_hash: TxHash) -
     .context("Failed to finish tx processing")?;
     Ok(())
 }
+
+#[instrument(skip(db))]
+pub async fn finish_tx_cleanup<T: ConnectionTrait>(db: &T, tx_hash: TxHash) -> Result<()> {
+    let tx_hash: Vec<u8> = tx_hash.as_slice().into();
+    db.execute(Statement::from_sql_and_values(
+        db.get_database_backend(),
+        "delete from golem_base_pending_transaction_cleanups where hash = $1",
+        [tx_hash.into()],
+    ))
+    .await
+    .context("Failed to finish tx cleanup")?;
+    Ok(())
+}
