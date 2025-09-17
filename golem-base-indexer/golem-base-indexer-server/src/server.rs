800c7163e099cfe9d8300e508f5f285d9633fe2e
Radek Bochenek <radekb@mac.com>
2025-09-17 09:00:29 +0200

    feat: timeseries update task

--- golem-base-indexer/golem-base-indexer-server/src/server.rs
+++ golem-base-indexer/golem-base-indexer-server/src/server.rs
@@ -3,7 +3,7 @@ use crate::{
         golem_base_indexer_service_actix::route_golem_base_indexer_service,
         health_actix::route_health, health_server::HealthServer,
     },
-    services::{GolemBaseIndexerService, HealthService},
+    services::{GolemBaseIndexerService, HealthService, UpdateTimeseriesService},
     settings::Settings,
 };
 use blockscout_endpoint_swagger::route_swagger;
@@ -61,6 +61,9 @@ pub async fn run(
     let health = Arc::new(HealthService::default());
 
     // TODO: init services here
+
+    let update_timeseries = UpdateTimeseriesService::new(Arc::clone(&db_connection));
+    update_timeseries.spawn_periodic_task(1800); // 1800 seconds = 30 minutes
 
     let golem_base_indexer = Arc::new(GolemBaseIndexerService::new(db_connection));
 
---
+++ golem-base-indexer/golem-base-indexer-server/src/services/update_timeseries.rs
@@ -0,0 +1,44 @@
+use anyhow::Result;
+use sea_orm::{ConnectionTrait, DatabaseBackend, DatabaseConnection, Statement};
+use std::sync::Arc;
+use std::time::Duration;
+use tokio::time;
+use tracing::error;
+
+#[derive(Clone)]
+pub struct UpdateTimeseriesService {
+    db: Arc<DatabaseConnection>,
+}
+
+impl UpdateTimeseriesService {
+    pub fn new(db: Arc<DatabaseConnection>) -> Self {
+        Self { db }
+    }
+
+    pub async fn refresh_timeseries(&self) -> Result<()> {
+        self.db
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "REFRESH MATERIALIZED VIEW golem_base_timeseries",
+            ))
+            .await?;
+
+        Ok(())
+    }
+
+    pub fn spawn_periodic_task(&self, interval_seconds: u64) {
+        let refresher = self.clone();
+
+        tokio::spawn(async move {
+            let mut interval = time::interval(Duration::from_secs(interval_seconds));
+
+            loop {
+                interval.tick().await;
+
+                if let Err(e) = refresher.refresh_timeseries().await {
+                    error!("Timeseries refresh failed: {}", e);
+                }
+            }
+        });
+    }
+}
