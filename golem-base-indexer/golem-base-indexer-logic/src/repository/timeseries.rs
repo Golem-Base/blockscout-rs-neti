cd2ff73dfb9d6c85b018edc1a365175747e2befa
Radek Bochenek <radekb@mac.com>
2025-09-28 20:06:23 +0200

    feat(wip): storage forecast

--- golem-base-indexer/golem-base-indexer-logic/src/repository/timeseries.rs
+++ golem-base-indexer/golem-base-indexer-logic/src/repository/timeseries.rs
@@ -44,6 +44,25 @@ struct DbChartDataUsageHourly {
     pub active_data_bytes: i64,
 }
 
+#[derive(Iden)]
+pub enum GolemBaseTimeseriesStorageForecast {
+    Table,
+    Timestamp,
+    TotalStorage,
+}
+
+#[derive(Debug, FromQueryResult)]
+struct DbChartStorageForecastDaily {
+    pub timestamp: NaiveDate,
+    pub total_storage: i64,
+}
+
+#[derive(Debug, FromQueryResult)]
+struct DbChartStorageForecastHourly {
+    pub timestamp: NaiveDateTime,
+    pub total_storage: i64,
+}
+
 #[instrument(skip(db))]
 pub async fn timeseries_data_usage<T: ConnectionTrait>(
     db: &T,
@@ -54,7 +73,7 @@ pub async fn timeseries_data_usage<T: ConnectionTrait>(
     let chart = match resolution {
         ChartResolution::Day => {
             let (from_date, to_date) = parse_date_range(from, to)?;
-            let query = build_daily_data_usage_query(from_date, to_date);
+            let query = build_query_data_usage_daily(from_date, to_date);
             let results = DbChartDataUsageDaily::find_by_statement(Statement::from_string(
                 DbBackend::Postgres,
                 query.to_string(PostgresQueryBuilder),
@@ -64,7 +83,7 @@ pub async fn timeseries_data_usage<T: ConnectionTrait>(
             .context("Failed to get data usage timeseries")?;
 
             let initial_value = if let Some(from_date) = from_date {
-                let lookback_query = build_daily_value_before_query(from_date);
+                let lookback_query = build_query_data_usage_daily_last_value(from_date);
                 let lookback_result =
                     DbChartDataUsageDaily::find_by_statement(Statement::from_string(
                         DbBackend::Postgres,
@@ -79,11 +98,11 @@ pub async fn timeseries_data_usage<T: ConnectionTrait>(
                 None
             };
 
-            generate_daily_points(results, from_date, to_date, initial_value)?
+            generate_points_data_usage_daily(results, from_date, to_date, initial_value)?
         }
         ChartResolution::Hour => {
             let (from_datetime, to_datetime) = parse_datetime_range(from, to)?;
-            let query = build_hourly_data_usage_query(from_datetime, to_datetime);
+            let query = build_query_data_usage_hourly(from_datetime, to_datetime);
             let results = DbChartDataUsageHourly::find_by_statement(Statement::from_string(
                 DbBackend::Postgres,
                 query.to_string(PostgresQueryBuilder),
@@ -93,7 +112,7 @@ pub async fn timeseries_data_usage<T: ConnectionTrait>(
             .context("Failed to get data usage timeseries")?;
 
             let initial_value = if let Some(from_dt) = from_datetime {
-                let lookback_query = build_hourly_last_value_before_query(from_dt);
+                let lookback_query = build_query_data_usage_hourly_last_value(from_dt);
                 let lookback_result =
                     DbChartDataUsageHourly::find_by_statement(Statement::from_string(
                         DbBackend::Postgres,
@@ -108,7 +127,7 @@ pub async fn timeseries_data_usage<T: ConnectionTrait>(
                 None
             };
 
-            generate_hourly_points(results, from_datetime, to_datetime, initial_value)?
+            generate_points_data_usage_hourly(results, from_datetime, to_datetime, initial_value)?
         }
     };
 
@@ -116,6 +135,54 @@ pub async fn timeseries_data_usage<T: ConnectionTrait>(
         id: "golemBaseDataUsage".to_string(),
         title: "Data over time".to_string(),
         description: "Data storage over time".to_string(),
+    };
+
+    Ok((chart, info))
+}
+
+#[instrument(skip(db))]
+pub async fn timeseries_storage_forecast<T: ConnectionTrait>(
+    db: &T,
+    to: &str,
+    resolution: ChartResolution,
+) -> Result<(Vec<ChartPoint>, ChartInfo)> {
+    let chart = match resolution {
+        ChartResolution::Day => {
+            let (_, to_date) = parse_date_range(None, Some(to.to_string()))?;
+            let query = build_query_storage_forecast_daily(to_date.unwrap());
+            let results = DbChartStorageForecastDaily::find_by_statement(Statement::from_string(
+                DbBackend::Postgres,
+                query.to_string(PostgresQueryBuilder),
+            ))
+            .all(db)
+            .await
+            .context("Failed to get storage forecast timeseries")?;
+
+            tracing::warn!("results = {:#?}", results);
+
+            generate_points_storage_forecast_daily(results, to_date.unwrap())?
+        }
+        ChartResolution::Hour => {
+            let (_, to_datetime) = parse_datetime_range(None, Some(to.to_string()))?;
+            let query = build_query_storage_forecast_hourly(to_datetime.unwrap());
+            let results = DbChartStorageForecastHourly::find_by_statement(Statement::from_string(
+                DbBackend::Postgres,
+                query.to_string(PostgresQueryBuilder),
+            ))
+            .all(db)
+            .await
+            .context("Failed to get storage forecast timeseries")?;
+
+            tracing::warn!("results = {:#?}", results);
+
+            generate_points_storage_forecast_hourly(results, to_datetime.unwrap())?
+        }
+    };
+
+    let info = ChartInfo {
+        id: "golemBaseStorageForecast".to_string(),
+        title: "Storage forecast".to_string(),
+        description: "Chain storage forecast".to_string(),
     };
 
     Ok((chart, info))
@@ -173,7 +240,7 @@ fn parse_datetime_range(
             let parsed = NaiveDateTime::parse_from_str(&datetime_str, "%Y-%m-%d %H:%M")
                 .map_err(|e| anyhow!("Invalid to datetime format: {}", e))?;
 
-            Some(if parsed > current_datetime {
+            Some(if from_datetime.is_some() && parsed > current_datetime {
                 current_datetime
             } else {
                 parsed
@@ -195,7 +262,7 @@ fn parse_datetime_range(
     Ok((from_datetime, to_datetime))
 }
 
-fn build_daily_value_before_query(before_date: NaiveDate) -> SelectStatement {
+fn build_query_data_usage_daily_last_value(before_date: NaiveDate) -> SelectStatement {
     Query::select()
         .expr_as(
             Expr::col(GolemBaseTimeseriesDataUsage::Timestamp).cast_as("date"),
@@ -217,7 +284,7 @@ fn build_daily_value_before_query(before_date: NaiveDate) -> SelectStatement {
         .to_owned()
 }
 
-fn build_daily_data_usage_query(from: Option<NaiveDate>, to: Option<NaiveDate>) -> SelectStatement {
+fn build_query_data_usage_daily(from: Option<NaiveDate>, to: Option<NaiveDate>) -> SelectStatement {
     let mut query = Query::select()
         .expr_as(
             Expr::col(GolemBaseTimeseriesDataUsage::Timestamp).cast_as("date"),
@@ -260,7 +327,94 @@ fn build_daily_data_usage_query(from: Option<NaiveDate>, to: Option<NaiveDate>) 
     query
 }
 
-fn generate_daily_points(
+fn build_query_data_usage_hourly_last_value(before_datetime: NaiveDateTime) -> SelectStatement {
+    Query::select()
+        .columns([
+            GolemBaseTimeseriesDataUsage::Timestamp,
+            GolemBaseTimeseriesDataUsage::ActiveDataBytes,
+        ])
+        .from(GolemBaseTimeseriesDataUsage::Table)
+        .and_where(Expr::col(GolemBaseTimeseriesDataUsage::Timestamp).lt(before_datetime))
+        .order_by(
+            GolemBaseTimeseriesDataUsage::Timestamp,
+            sea_query::Order::Desc,
+        )
+        .limit(1)
+        .to_owned()
+}
+
+fn build_query_data_usage_hourly(
+    from: Option<NaiveDateTime>,
+    to: Option<NaiveDateTime>,
+) -> SelectStatement {
+    let mut query = Query::select()
+        .columns([
+            GolemBaseTimeseriesDataUsage::Timestamp,
+            GolemBaseTimeseriesDataUsage::ActiveDataBytes,
+        ])
+        .from(GolemBaseTimeseriesDataUsage::Table)
+        .order_by(
+            GolemBaseTimeseriesDataUsage::Timestamp,
+            sea_query::Order::Asc,
+        )
+        .to_owned();
+
+    match (from, to) {
+        (Some(from_datetime), Some(to_datetime)) => {
+            query.and_where(
+                Expr::col(GolemBaseTimeseriesDataUsage::Timestamp)
+                    .between(from_datetime, to_datetime),
+            );
+        }
+        (Some(from_datetime), None) => {
+            query.and_where(Expr::col(GolemBaseTimeseriesDataUsage::Timestamp).gte(from_datetime));
+        }
+        (None, Some(to_datetime)) => {
+            query.and_where(Expr::col(GolemBaseTimeseriesDataUsage::Timestamp).lte(to_datetime));
+        }
+        (None, None) => {}
+    }
+
+    query
+}
+
+fn build_query_storage_forecast_daily(to_date: NaiveDate) -> SelectStatement {
+    Query::select()
+        .expr_as(
+            Expr::col(GolemBaseTimeseriesStorageForecast::Timestamp).cast_as("date"),
+            "timestamp",
+        )
+        .expr_as(
+            Expr::max(Expr::col(GolemBaseTimeseriesStorageForecast::TotalStorage)),
+            GolemBaseTimeseriesStorageForecast::TotalStorage,
+        )
+        .from(GolemBaseTimeseriesStorageForecast::Table)
+        .and_where(
+            Expr::col(GolemBaseTimeseriesStorageForecast::Timestamp)
+                .cast_as("date")
+                .lte(to_date),
+        )
+        .group_by_col("timestamp")
+        .order_by("timestamp", sea_query::Order::Desc)
+        .to_owned()
+}
+
+fn build_query_storage_forecast_hourly(to_datetime: NaiveDateTime) -> SelectStatement {
+    Query::select()
+        .columns([
+            GolemBaseTimeseriesStorageForecast::Timestamp,
+            GolemBaseTimeseriesStorageForecast::TotalStorage,
+        ])
+        .from(GolemBaseTimeseriesStorageForecast::Table)
+        .and_where(Expr::col(GolemBaseTimeseriesStorageForecast::Timestamp).lte(to_datetime))
+        .order_by(
+            GolemBaseTimeseriesStorageForecast::Timestamp,
+            sea_query::Order::Asc,
+        )
+        .to_owned()
+}
+
+fn generate_points_data_usage_daily(
     db_results: Vec<DbChartDataUsageDaily>,
     from_date: Option<NaiveDate>,
     to_date: Option<NaiveDate>,
@@ -316,58 +470,7 @@ fn generate_daily_points(
     Ok(points)
 }
 
-fn build_hourly_last_value_before_query(before_datetime: NaiveDateTime) -> SelectStatement {
-    Query::select()
-        .columns([
-            GolemBaseTimeseriesDataUsage::Timestamp,
-            GolemBaseTimeseriesDataUsage::ActiveDataBytes,
-        ])
-        .from(GolemBaseTimeseriesDataUsage::Table)
-        .and_where(Expr::col(GolemBaseTimeseriesDataUsage::Timestamp).lt(before_datetime))
-        .order_by(
-            GolemBaseTimeseriesDataUsage::Timestamp,
-            sea_query::Order::Desc,
-        )
-        .limit(1)
-        .to_owned()
-}
-
-fn build_hourly_data_usage_query(
-    from: Option<NaiveDateTime>,
-    to: Option<NaiveDateTime>,
-) -> SelectStatement {
-    let mut query = Query::select()
-        .columns([
-            GolemBaseTimeseriesDataUsage::Timestamp,
-            GolemBaseTimeseriesDataUsage::ActiveDataBytes,
-        ])
-        .from(GolemBaseTimeseriesDataUsage::Table)
-        .order_by(
-            GolemBaseTimeseriesDataUsage::Timestamp,
-            sea_query::Order::Asc,
-        )
-        .to_owned();
-
-    match (from, to) {
-        (Some(from_datetime), Some(to_datetime)) => {
-            query.and_where(
-                Expr::col(GolemBaseTimeseriesDataUsage::Timestamp)
-                    .between(from_datetime, to_datetime),
-            );
-        }
-        (Some(from_datetime), None) => {
-            query.and_where(Expr::col(GolemBaseTimeseriesDataUsage::Timestamp).gte(from_datetime));
-        }
-        (None, Some(to_datetime)) => {
-            query.and_where(Expr::col(GolemBaseTimeseriesDataUsage::Timestamp).lte(to_datetime));
-        }
-        (None, None) => {}
-    }
-
-    query
-}
-
-fn generate_hourly_points(
+fn generate_points_data_usage_hourly(
     db_results: Vec<DbChartDataUsageHourly>,
     from_datetime: Option<NaiveDateTime>,
     to_datetime: Option<NaiveDateTime>,
@@ -418,6 +521,96 @@ fn generate_hourly_points(
         });
 
         current_time = next_hour;
+    }
+
+    Ok(points)
+}
+
+fn generate_points_storage_forecast_hourly(
+    db_results: Vec<DbChartStorageForecastHourly>,
+    to_datetime: NaiveDateTime,
+) -> Result<Vec<ChartPoint>> {
+    if db_results.is_empty() {
+        return Err(anyhow!("No data usage available"));
+    }
+
+    let first_entry = &db_results[0];
+    let initial_value = first_entry.total_storage;
+    let start_time = first_entry.timestamp;
+
+    let data_map: HashMap<NaiveDateTime, i64> = db_results
+        .into_iter()
+        .skip(1)
+        .map(|row| (row.timestamp, row.total_storage))
+        .collect();
+
+    let mut points = Vec::new();
+    let mut current_time = start_time;
+    let mut last_known_value = initial_value;
+
+    while current_time < to_datetime {
+        let next_hour = current_time + Duration::hours(1);
+
+        let value = match data_map.get(&current_time) {
+            Some(&actual_value) => {
+                last_known_value = actual_value;
+                actual_value
+            }
+            None => last_known_value,
+        };
+
+        points.push(ChartPoint {
+            date: current_time.format("%Y-%m-%d %H:%M").to_string(),
+            date_to: next_hour.format("%Y-%m-%d %H:%M").to_string(),
+            value: value.to_string(),
+        });
+
+        current_time = next_hour;
+    }
+
+    Ok(points)
+}
+
+fn generate_points_storage_forecast_daily(
+    db_results: Vec<DbChartStorageForecastDaily>,
+    to_datetime: NaiveDate,
+) -> Result<Vec<ChartPoint>> {
+    if db_results.is_empty() {
+        return Err(anyhow!("No data usage available"));
+    }
+
+    let first_entry = &db_results[0];
+    let initial_value = first_entry.total_storage;
+    let start_time = first_entry.timestamp;
+
+    let data_map: HashMap<NaiveDate, i64> = db_results
+        .into_iter()
+        .skip(1)
+        .map(|row| (row.timestamp, row.total_storage))
+        .collect();
+
+    let mut points = Vec::new();
+    let mut current_time = start_time;
+    let mut last_known_value = initial_value;
+
+    while current_time < to_datetime {
+        let next_day = current_time + Duration::days(1);
+
+        let value = match data_map.get(&current_time) {
+            Some(&actual_value) => {
+                last_known_value = actual_value;
+                actual_value
+            }
+            None => last_known_value,
+        };
+
+        points.push(ChartPoint {
+            date: current_time.format("%Y-%m-%d").to_string(),
+            date_to: next_day.format("%Y-%m-%d").to_string(),
+            value: value.to_string(),
+        });
+
+        current_time = next_day;
     }
 
     Ok(points)
