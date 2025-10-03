bcf1d95e7abbd85ec3a788433b3a07a41b9ab4f7
Radek Bochenek <radekb@mac.com>
2025-10-03 19:39:33 +0800

    fix: remove duplicate tests

--- golem-base-indexer/golem-base-indexer-server/tests/tests/mod.rs
+++ golem-base-indexer/golem-base-indexer-server/tests/tests/mod.rs
@@ -1,6 +1,5 @@
 mod address_leaderboard_ranks;
 mod block_stats;
-mod chart_data_usage;
 mod expired_entities_annotations_get_deactivated;
 mod expired_entities_dont_get_updated;
 mod get_address_stats_endpoint_works;
