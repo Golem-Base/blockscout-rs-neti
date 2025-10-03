5ab3ec1140a3f187160c6c9c8b9fc51f1ad952fd
Radek Bochenek <radekb@mac.com>
2025-10-03 19:47:27 +0800

    fix: return points for no-data range

--- golem-base-indexer/golem-base-indexer-server/tests/tests/timeseries_data_usage.rs
+++ golem-base-indexer/golem-base-indexer-server/tests/tests/timeseries_data_usage.rs
@@ -1,10 +1,28 @@
 use crate::helpers;
 
 use blockscout_service_launcher::test_server;
-use golem_base_indexer_logic::Indexer;
+use golem_base_indexer_logic::{
+    types::{ChartInfo, ChartPoint},
+    Indexer,
+};
 use helpers::utils::refresh_timeseries;
 use serde_json::{json, Value};
 use std::sync::Arc;
+
+fn chart_info() -> Value {
+    json!(ChartInfo {
+        id: "golemBaseDataUsage".to_string(),
+        title: "Data over time".to_string(),
+        description: "Data storage over time".to_string(),
+    })
+}
+
+fn endpoint_for_resolution_and_dates(resolution: &str, from: &str, to: &str) -> String {
+    format!(
+        "/api/v1/chart/data-usage?resolution={}&from={}&to={}",
+        resolution, from, to
+    )
+}
 
 #[tokio::test]
 #[ignore = "Needs database to run"]
@@ -24,23 +42,19 @@ async fn chart_data_usage_should_work() {
     // Hourly
     let response: Value = test_server::send_get_request(
         &base,
-        "/api/v1/chart/data-usage?resolution=HOUR&from=2025-07-22%2011:00&to=2025-07-22%2012:00",
+        &endpoint_for_resolution_and_dates("HOUR", "2025-07-22%2011:00", "2025-07-22%2012:00"),
     )
     .await;
 
+    let points = vec![ChartPoint {
+        date: "2025-07-22 11:00".to_string(),
+        date_to: "2025-07-22 12:00".to_string(),
+        value: "146".to_string(),
+    }];
+
     let expected: Value = json!({
-        "info": {
-            "id": "golemBaseDataUsage",
-            "title": "Data over time",
-            "description": "Data storage over time",
-        },
-        "chart": [
-            {
-                "date": "2025-07-22 11:00",
-                "date_to": "2025-07-22 12:00",
-                "value": "146",
-            }
-        ]
+        "info": chart_info(),
+        "chart": points,
     });
 
     assert_eq!(response, expected);
@@ -48,23 +62,88 @@ async fn chart_data_usage_should_work() {
     // Daily
     let response: Value = test_server::send_get_request(
         &base,
-        "/api/v1/chart/data-usage?resolution=DAY&from=2025-07-22&to=2025-07-23",
+        &endpoint_for_resolution_and_dates("DAY", "2025-07-22", "2025-07-23"),
     )
     .await;
 
+    let points = vec![ChartPoint {
+        date: "2025-07-22".to_string(),
+        date_to: "2025-07-23".to_string(),
+        value: "146".to_string(),
+    }];
+
     let expected: Value = json!({
-        "info": {
-            "id": "golemBaseDataUsage",
-            "title": "Data over time",
-            "description": "Data storage over time",
+        "info": chart_info(),
+        "chart": points,
+    });
+
+    assert_eq!(response, expected);
+}
+
+#[tokio::test]
+#[ignore = "Needs database to run"]
+async fn chart_data_usage_should_work_without_data_indexed() {
+    // Setup
+    let db = helpers::init_db("test", "chart_data_usage_should_work_without_data_indexed").await;
+    let client = db.client();
+    let base = helpers::init_golem_base_indexer_server(db, |x| x).await;
+
+    Indexer::new(client.clone(), Default::default())
+        .tick()
+        .await
+        .unwrap();
+    refresh_timeseries(Arc::clone(&client)).await.unwrap();
+
+    // Daily
+    let response: Value = test_server::send_get_request(
+        &base,
+        &endpoint_for_resolution_and_dates("DAY", "2025-10-01", "2025-10-03"),
+    )
+    .await;
+
+    let points = vec![
+        ChartPoint {
+            date: "2025-10-01".to_string(),
+            date_to: "2025-10-02".to_string(),
+            value: "0".to_string(),
         },
-        "chart": [
-            {
-                "date": "2025-07-22",
-                "date_to": "2025-07-23",
-                "value": "146",
-            }
-        ]
+        ChartPoint {
+            date: "2025-10-02".to_string(),
+            date_to: "2025-10-03".to_string(),
+            value: "0".to_string(),
+        },
+    ];
+
+    let expected: Value = json!({
+        "info": chart_info(),
+        "chart": points,
+    });
+
+    assert_eq!(response, expected);
+
+    // Hourly
+    let response: Value = test_server::send_get_request(
+        &base,
+        &endpoint_for_resolution_and_dates("HOUR", "2025-10-01%2010:00", "2025-10-01%2012:00"),
+    )
+    .await;
+
+    let points = vec![
+        ChartPoint {
+            date: "2025-10-01 10:00".to_string(),
+            date_to: "2025-10-01 11:00".to_string(),
+            value: "0".to_string(),
+        },
+        ChartPoint {
+            date: "2025-10-01 11:00".to_string(),
+            date_to: "2025-10-01 12:00".to_string(),
+            value: "0".to_string(),
+        },
+    ];
+
+    let expected: Value = json!({
+        "info": chart_info(),
+        "chart": points,
     });
 
     assert_eq!(response, expected);
