f9ef4edf3d9e94e7167bbf84265ef69e68936847
Mateusz Juszczyk <matixovi@gmail.com>
2025-10-27 21:51:23 +0100

    Proto & module

--- golem-base-indexer/golem-base-indexer-logic/src/repository/timeseries/mod.rs
+++ golem-base-indexer/golem-base-indexer-logic/src/repository/timeseries/mod.rs
@@ -2,5 +2,6 @@ pub mod block_operations;
 pub mod block_transactions;
 mod common;
 pub mod data_usage;
+pub mod entity_count;
 pub mod operation_count;
 pub mod storage_forecast;
--- golem-base-indexer/golem-base-indexer-migration/src/lib.rs
+++ golem-base-indexer/golem-base-indexer-migration/src/lib.rs
@@ -32,6 +32,7 @@ mod m20251003_112845_create_mview_timeseries_operations_count;
 mod m20251010_082748_create_mview_timeseries_operations_count_by_type;
 mod m20251016_085731_migrate_logs_queue;
 mod m20251021_091030_cleanup_noop_txs_from_tx_queue;
+mod m20251024_100000_create_mview_timeseries_entity_count;
 
 pub struct Migrator;
 
@@ -72,6 +73,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20251010_082748_create_mview_timeseries_operations_count_by_type::Migration),
             Box::new(m20251016_085731_migrate_logs_queue::Migration),
             Box::new(m20251021_091030_cleanup_noop_txs_from_tx_queue::Migration),
+            Box::new(m20251024_100000_create_mview_timeseries_entity_count::Migration),
         ]
     }
 
