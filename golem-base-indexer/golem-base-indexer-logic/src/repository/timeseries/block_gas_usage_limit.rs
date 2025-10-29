b34f3165a625f2df0a39072d95a1b9c41603f10d
Radek Bochenek <radekb@mac.com>
2025-10-29 13:43:51 +0100

    feat: replace materialized view with blocks sea-query

--- golem-base-indexer/golem-base-indexer-logic/src/repository/timeseries/block_gas_usage_limit.rs
+++ golem-base-indexer/golem-base-indexer-logic/src/repository/timeseries/block_gas_usage_limit.rs
@@ -1,24 +1,16 @@
 use anyhow::{Context, Result};
-use sea_orm::{prelude::*, DbBackend, FromQueryResult, Statement};
-use sea_query::{Iden, PostgresQueryBuilder, Query};
+use golem_base_indexer_entity::blocks;
+use sea_orm::{prelude::*, FromQueryResult, QueryOrder, QuerySelect};
+use sea_query::{Alias, ExprTrait, Func};
 use tracing::instrument;
 
 use crate::types::{BlockGasUsageLimitPoint, ChartInfo};
 
-#[derive(Iden)]
-pub enum GolemBaseTimeseriesBlockGasUsedAndLimit {
-    Table,
-    BlockNumber,
-    GasUsed,
-    GasLimit,
-    GasUsagePercentage,
-}
-
 #[derive(Debug, FromQueryResult)]
 struct DbChartBlockGasUsageLimit {
     pub block_number: i64,
-    pub gas_used: i64,
-    pub gas_limit: i64,
+    pub gas_used: Decimal,
+    pub gas_limit: Decimal,
     pub gas_usage_percentage: f64,
 }
 
@@ -39,31 +31,35 @@ impl TryFrom<DbChartBlockGasUsageLimit> for BlockGasUsageLimitPoint {
 pub async fn timeseries_block_gas_usage_limit<T: ConnectionTrait>(
     db: &T,
 ) -> Result<(Vec<BlockGasUsageLimitPoint>, ChartInfo)> {
-    let query = Query::select()
-        .columns([
-            GolemBaseTimeseriesBlockGasUsedAndLimit::BlockNumber,
-            GolemBaseTimeseriesBlockGasUsedAndLimit::GasUsed,
-            GolemBaseTimeseriesBlockGasUsedAndLimit::GasLimit,
-            GolemBaseTimeseriesBlockGasUsedAndLimit::GasUsagePercentage,
-        ])
-        .from(GolemBaseTimeseriesBlockGasUsedAndLimit::Table)
-        .order_by(
-            GolemBaseTimeseriesBlockGasUsedAndLimit::BlockNumber,
-            sea_query::Order::Asc,
+    let points = blocks::Entity::find()
+        .select_only()
+        .column_as(blocks::Column::Number, "block_number")
+        .column(blocks::Column::GasUsed)
+        .column(blocks::Column::GasLimit)
+        .column_as(
+            Expr::case(
+                Expr::col(blocks::Column::GasLimit).gt(0),
+                Func::round_with_precision(
+                    Expr::col(blocks::Column::GasUsed)
+                        .div(Expr::col(blocks::Column::GasLimit))
+                        .mul(100),
+                    2,
+                ),
+            )
+            .finally(0)
+            .cast_as(Alias::new("DOUBLE PRECISION")),
+            "gas_usage_percentage",
         )
-        .to_owned();
-
-    let points = DbChartBlockGasUsageLimit::find_by_statement(Statement::from_string(
-        DbBackend::Postgres,
-        query.to_string(PostgresQueryBuilder),
-    ))
-    .all(db)
-    .await
-    .context("Failed to get block gas usage and limit timeseries")?
-    .into_iter()
-    .map(|db_point| db_point.try_into())
-    .collect::<Result<Vec<BlockGasUsageLimitPoint>>>()
-    .context("Failed to convert chart data for block gas usage and limit timeseries")?;
+        .order_by_desc(blocks::Column::Number)
+        .into_model::<DbChartBlockGasUsageLimit>()
+        .all(db)
+        .await
+        .context("Failed to get block gas usage and limit timeseries")?
+        .into_iter()
+        .rev()
+        .map(|db_point| db_point.try_into())
+        .collect::<Result<Vec<BlockGasUsageLimitPoint>>>()
+        .context("Failed to convert chart data for block gas usage and limit timeseries")?;
 
     let info = ChartInfo {
         id: "golemBaseBlockGasUsageLimit".to_string(),
